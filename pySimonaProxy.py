274305cc064daeca1523d7bbb2b15369154dd15d
petrs <petrsgit@gmail.com>
2018-08-02 19:55:25 +0200

    refactoring into externally importable class with configuration

--- pySimonaProxy.py
+++ pySimonaProxy.py
@@ -19,16 +19,6 @@ coloredlogs.install(level='DEBUG')
 
 logging.basicConfig(level=logging.DEBUG)
 
-TEST_SIMULATED_CARD = False # if true, simulated response is send back
-
-HOST = ''   # Symbolic name meaning all available interfaces
-PORT = 4001 # Arbitrary non-privileged port
-
-GPPROREST_PROXY = 'http://127.0.0.1:8081/api/v1/basic'  # rest proxy for simona boards, use basicj for more info
-GPPROREST_TEST_WITH_LOCAL_READER = True # if true, name of local reader will be used instead of supplied remote one
-GPPROREST_TEST_LOCAL_READER = 'Generic EMV Smartcard Reader 0'
-headers = {'X-Auth-Token': 'b', 'Connection': 'close'}
-
 CMD_APDU = "APDU"
 CMD_RESET = "RESET"
 CMD_ENUM = "ENUM"
@@ -36,6 +26,21 @@ CMD_SEPARATOR = ":"
 CMD_LINE_SEPARATOR = "|"
 CMD_RESPONSE_END = "@@"
 CMD_RESPONSE_FAIL = "FAIL"
+
+
+class ProxyConfig:
+    test_simulated_card = False # if true, simulated response is send back
+
+    socket_host = ''   # Symbolic name meaning all available interfaces
+    socket_port = 4001 # Arbitrary non-privileged port
+
+    gpprorest_proxy = 'http://127.0.0.1:8081/api/v1/basic'  # rest proxy for simona boards, use basicj for more info
+    gpprorest_http_headers = {'X-Auth-Token': 'b'}
+
+    gpprorest_test_with_local_reader = True # if true, name of local reader will be used instead of supplied remote one
+    # gpprorest_test_local_reader = 'Generic EMV Smartcard Reader 0'
+    gpprorest_test_local_reader = 'OMNIKEY CardMan 6121 0'
+
 
 
 class InputRequestData:
@@ -51,143 +56,149 @@ class InputRequestData:
         self.command_data = command_data
 
 
-# Function for pasing input request
-# ><reader name>|><cmd ID>:<"APDU" / "RESET" / "ENUM">:<optional hexa string, e.g. "00A4040304">|
-def parse_input_request(s):
-    match = re.match(r'>(?P<readerName>.*?)\|>(?P<commandID>.*?):(?P<commandName>.*?):(?P<commandData>.*?)\|', s, re.I)
-    if match:
-        command_data = match.group("commandData")
-        command_data = command_data.replace(' ', '')
-        return InputRequestData(match.group("readerName"), match.group("commandID"),
-                                match.group("commandName"), command_data)
-    else:
-        return None
+class SimonaSocketProxy:
+    # Function for pasing input request
+    # ><reader name>|><cmd ID>:<"APDU" / "RESET" / "ENUM">:<optional hexa string, e.g. "00A4040304">|
+    @staticmethod
+    def parse_input_request(s):
+        match = re.match(r'>(?P<readerName>.*?)\|>(?P<commandID>.*?):(?P<commandName>.*?):(?P<commandData>.*?)\|', s, re.I)
+        if match:
+            command_data = match.group("commandData")
+            command_data = command_data.replace(' ', '')
+            return InputRequestData(match.group("readerName"), match.group("commandID"),
+                                    match.group("commandName"), command_data)
+        else:
+            return None
 
-def make_request(proxy, payload, headers):
-    response_data = None
-    try:
-        logging.debug('Going to to send REST request to GPProREST proxy...')
-        r = requests.get(proxy, params=payload, headers=headers)
-    except ConnectionError as e:
-        logging.error('Problem with connection' + e)
-    except:
-        logging.error('Problem with connection')
-    else:
-        # process response
-        logging.debug('Response received')
-        for line in r.content.decode().splitlines():
-            if line != 'null' and len(line) > 0:
-                response_data = line
-                break
-        r.close()
+    @staticmethod
+    def make_request(proxy, payload, headers):
+        response_data = None
+        try:
+            logging.debug('Going to to send REST request to GPProREST proxy...')
+            r = requests.get(proxy, params=payload, headers=headers)
+        except ConnectionError as e:
+            logging.error('Problem with connection' + e)
+        except:
+            logging.error('Problem with connection')
+        else:
+            # process response
+            logging.debug('Response received: ' + r.content.decode())
+            for line in r.content.decode().splitlines():
+                if line != 'null' and len(line) > 0:
+                    response_data = line
+                    break
+            r.close()
 
-    return response_data
+        return response_data
 
+    # Function for handling connections. This will be used to create threads
+    @staticmethod
+    def client_thread(connection, ip, port, proxy_cfg):
+        try:
+            # infinite loop so that function do not terminate and thread do not end.
+            while True:
+                # Receiving from client
+                data = connection.recv(4096)
 
-# Function for handling connections. This will be used to create threads
-def client_thread(connection, ip, port):
-    try:
-        # infinite loop so that function do not terminate and thread do not end.
-        while True:
-            # Receiving from client
-            data = connection.recv(4096)
+                if len(data) == 0:
+                    time.sleep(0.1) # sleep little before making another receive attempt
+                    continue
 
-            if len(data) == 0:
-                time.sleep(0.1) # sleep little before making another receive attempt
-                continue
+                value = data.decode("utf-8")
+                value = value.strip()
 
-            value = data.decode("utf-8")
-            value = value.strip()
+                logging.debug(">> " + value)
 
-            logging.debug(">> " + value)
+                # parse input
+                input_req = SimonaSocketProxy.parse_input_request(value)
 
-            # parse input
-            input_req = parse_input_request(value)
+                if not input_req:
+                    logging.error('Invalid input request, skipping')
+                    continue
 
-            if not input_req:
-                logging.error('Invalid input request, skipping')
-                continue
+                logging.info("Reader:'{0}',CommandID:'{1}',Command:'{2}',Data:'{3}'".format(input_req.reader_name,
+                                                                                            input_req.command_id,
+                                                                                            input_req.command_name,
+                                                                                            input_req.command_data))
+                # for testing with local card, rename Simona readers to local one
+                if proxy_cfg.gpprorest_test_with_local_reader:
+                    if input_req.reader_name.find('Simona') != -1:
+                        logging.debug('Changing remote reader {0} to local reader {1} for testing'
+                                      .format(input_req.reader_name, proxy_cfg.gpprorest_test_local_reader))
+                        input_req.reader_name = proxy_cfg.gpprorest_test_local_reader
 
-            logging.info("Reader: '{0}', CommandID: '{1}', Command: '{2}', CommandData: '{3}'".format(input_req.reader_name,
-                                                                                               input_req.command_id,
-                                                                                               input_req.command_name,
-                                                                                               input_req.command_data))
-            # for testing with local card, rename Simona readers to local one
-            if GPPROREST_TEST_WITH_LOCAL_READER:
-                if input_req.reader_name.find('Simona') != -1:
-                    logging.debug('Changing remote reader {0} to local reader {1} for testing'.format(input_req.reader_name, GPPROREST_TEST_LOCAL_READER))
-                    input_req.reader_name = GPPROREST_TEST_LOCAL_READER
+                response_data = None
 
-            response_created = False
-            response_data = None
+                # SEND APDU
+                if input_req.command_name.lower() == CMD_APDU.lower():
+                    if proxy_cfg.test_simulated_card:
+                        response_data = "102030409000"
+                    else:
+                        payload = {'apdu': input_req.command_data, 'terminal': input_req.reader_name}
+                        response_data = SimonaSocketProxy.make_request(proxy_cfg.gpprorest_proxy, payload, proxy_cfg.gpprorest_http_headers)
 
-            # SEND APDU
-            if input_req.command_name.lower() == CMD_APDU.lower():
-                if TEST_SIMULATED_CARD:
-                    response_data = "102030409000"
-                else:
-                    payload = {'apdu': input_req.command_data, 'terminal': input_req.reader_name}
-                    response_data = make_request(GPPROREST_PROXY, payload, headers)
+                # RESET
+                if input_req.command_name.lower() == CMD_RESET.lower():
+                    if proxy_cfg.test_simulated_card:
+                        # test response, send to SIMONA instead
+                        response_data = "621A82013883023F008404524F4F5485030079AD8A0105A1038B01019000"
+                    else:
+                        payload = {'reset': '1', 'terminal': input_req.reader_name, 'close': '1'}
+                        response_data = SimonaSocketProxy.make_request(proxy_cfg.gpprorest_proxy, payload, proxy_cfg.gpprorest_http_headers)
 
-            # RESET
-            if input_req.command_name.lower() == CMD_RESET.lower():
-                if TEST_SIMULATED_CARD:
-                    # test response, send to SIMONA instead
-                    response_data = "621A82013883023F008404524F4F5485030079AD8A0105A1038B01019000"
-                else:
-                    payload = {'reset': '1', 'terminal': input_req.reader_name}
-                    response_data = make_request(GPPROREST_PROXY, payload, headers)
+                # No valid command found
+                if not response_data:
+                    response_data = CMD_RESPONSE_FAIL
 
+                response = ">{0}{1}{2}{3}\n".format(input_req.command_id, CMD_SEPARATOR, response_data,
+                                                    CMD_RESPONSE_END)
+                logging.info(response)
+                connection.sendall(response.encode("utf-8"))
+        except:
+            logging.info('Exception in serving response, ending thread')
+            logging.info('\n')
 
-            # No valid command found
-            if not response_data:
-                response_data = CMD_RESPONSE_FAIL
+        # Terminate connection for given client (if outside loop)
+        connection.close()
+        return
 
-            response = ">{0}{1}{2}{3}\n".format(input_req.command_id, CMD_SEPARATOR, response_data,
-                                                CMD_RESPONSE_END)
-            logging.info(response)
-            connection.sendall(response.encode("utf-8"))
-    except:
-        logging.info('Exception in serving response, ending thread')
-        logging.info('\n')
+    @staticmethod
+    def start_server(proxy_cfg):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
+            # this is for easy starting/killing the app
+            soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            logging.debug('Socket created')
 
-    # Terminate connection for given client (if outside loop)
-    connection.close()
-    return
+            # Bind socket to local host and port
+            try:
+                soc.bind((proxy_cfg.socket_host, proxy_cfg.socket_port))
+                logging.debug('Socket bind complete')
+            except socket.error as msg:
+                logging.error('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+                sys.exit()
+
+            # Start listening on socket
+            soc.listen(10)
+            logging.info('Socket now listening')
+
+            from threading import Thread
+
+            # now keep talking with the client
+            while True:
+                # wait to accept a connection - blocking call
+                conn, addr = soc.accept()
+                ip, port = str(addr[0]), str(addr[1])
+                logging.info('Connected with ' + ip + ':' + port)
+
+                # start new thread takes with arguments
+                Thread(target=SimonaSocketProxy.client_thread, args=(conn, ip, port, proxy_cfg)).start()
+
+            soc.close()
 
 
 def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
-        # this is for easy starting/killing the app
-        soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        logging.debug('Socket created')
-
-        # Bind socket to local host and port
-        try:
-            soc.bind((HOST, PORT))
-            logging.debug('Socket bind complete')
-        except socket.error as msg:
-            logging.error('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-            sys.exit()
-
-
-        # Start listening on socket
-        soc.listen(10)
-        logging.info('Socket now listening')
-
-        from threading import Thread
-
-        # now keep talking with the client
-        while True:
-            # wait to accept a connection - blocking call
-            conn, addr = soc.accept()
-            ip, port = str(addr[0]), str(addr[1])
-            logging.info('Connected with ' + ip + ':' + port)
-
-            # start new thread takes with arguments
-            Thread(target=client_thread, args=(conn, ip, port)).start()
-
-        soc.close()
+    proxy_cfg = ProxyConfig()   # use default config
+    SimonaSocketProxy.start_server(proxy_cfg)
 
 if __name__ == "__main__":
     main()
